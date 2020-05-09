141d590e3d0701c15b0b5ff3cf6c3413188d3b00
Cristian Alvarado <ocdude@bluewavedigital.net>
2020-05-09 13:33:15 -0700

    fixed weird issues with config

--- timelapse.py
+++ timelapse.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
 	# load configuration
 	config = configparser.ConfigParser()
 	config.read('config.ini')
-	resolution = (config.get('camera','width'), config.get('camera','height'))
+	resolution = (int(config.get('camera','width')), int(config.get('camera','height')))
 	ssh_server = config.get('ssh','server')
 	ssh_port = config.get('ssh','port')
 	ssh_user = config.get('ssh','user')
