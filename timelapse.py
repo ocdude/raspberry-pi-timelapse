9c7cd71f679a62926695c6a68db8fffe774cb377
Cristian Alvarado <ocdude@bluewavedigital.net>
2020-05-09 13:37:43 -0700

    fixes for ssh

--- timelapse.py
+++ timelapse.py
@@ -21,9 +21,9 @@ def ssh_client(server, port, user, password):
 	client.connect(server, port, user, password)
 	return client
 
-def upload_image(image_path, ssh_client):
+def upload_image(image_path, remote_path, ssh_client):
 	scp_client = scp.SCPClient(ssh_client.get_transport())
-	scp.put(image_path)
+	scp_client.put(image_path)
 
 
 if __name__ == "__main__":
@@ -39,4 +39,4 @@ if __name__ == "__main__":
 	output = 'output.jpg'
 	take_picture(resolution, output)
 	client = ssh_client(ssh_server, ssh_port, ssh_user, ssh_password)
-	upload_image(output, client)
+	upload_image(output, ".", client)
