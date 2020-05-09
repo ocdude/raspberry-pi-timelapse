4d6fafe43844735cb416226de5b490c1219cc83b
Cristian Alvarado <ocdude@bluewavedigital.net>
2020-05-09 15:53:50 -0700

    fixing indentation

--- timelapse.py
+++ timelapse.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
 	elif mode == "continuous":
 		for filename in camera.capture_continuous(path.join(output,'img{counter:03d}.jpg')):
     	if upload is True:
-				# upload image
+    		# upload image
 				client = ssh_client(ssh_server, ssh_port, ssh_user, ssh_password)
 				upload_image(path.join(output,'%s' % filename), ssh_remote_path, client)
 			time.sleep(frequency)
